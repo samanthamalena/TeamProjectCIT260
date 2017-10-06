1a0d702a33a0316d62af663a7f921ecdf6c14817
SamanthaMalena <SamanthaMalena@SamanthaPC>
2017-10-06 14:22:22 -0600

    10/06/17 actor class

---
+++ TeamProjectCIT260/src/model/Actor.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author SamanthaMalena
+ */
+public class Actor {
+   private String name;
+   private String description;
+   private Player player;
+   private ArrayList<Game> games = new ArrayList<Game>();  
+   private ArrayList<Location> locations = new ArrayList<Location>();
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + Objects.hashCode(this.description);
+        hash = 29 * hash + Objects.hashCode(this.player);
+        hash = 29 * hash + Objects.hashCode(this.games);
+        hash = 29 * hash + Objects.hashCode(this.locations);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Actor other = (Actor) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.player, other.player)) {
+            return false;
+        }
+        if (!Objects.equals(this.games, other.games)) {
+            return false;
+        }
+        if (!Objects.equals(this.locations, other.locations)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" + "name=" + name + ", description=" + description + ", player=" + player + ", games=" + games + ", locations=" + locations + '}';
+    }
+
+    public Actor() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public <any> getGames() {
+        return games;
+    }
+
+    public void setGames(<any> games) {
+        this.games = games;
+    }
+
+    public <any> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(<any> locations) {
+        this.locations = locations;
+    }
+}
