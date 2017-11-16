f3bd5589999628a85d86dd5225d2e5cd0c8015a4
Me <Me@10.38.164.154>
2017-11-16 15:49:36 -0500

    Oct 6 2017 (named durped in Main)

--- TeamProjectCIT260/src/control/GameControl.java
+++ TeamProjectCIT260/src/control/GameControl.java
@@ -4,13 +4,15 @@
  * and open the template in the editor.
  */
 package control;
-import model.Player;
+import model.*;
+import teamprojectcit260.TeamProjectCIT260;
 
 /**
  *
  * @author Me
  */
 public class GameControl {
+
     public GameControl(){
     }
     public Player savePlayer(String playerName){
@@ -22,8 +24,36 @@ public class GameControl {
         
         return player;
     }
-    public void createNewGame(){
-        System.out.println("\n*****NewGameCreated*****");
+    
+    
+    public static int createNewGame(Player player){
+        if (player == null){
+            return -1;
+        }
+        Game game = new Game();
+        game.setPlayer(player);
+        TeamProjectCIT260.setCurrentGame(game);
+        Actor[] actors = GameControl.createActors();
+        game.setActors(actors);
+        Map map = createMap(1,1);
+        if (map == null){
+            return -1;
+        }
         
+        game.setMap(map);
+        return 1;
     }
+    
+    private static Actor[] createActors() {
+        Actor[] actors = new Actor[10];
+        actors[0].setName("Derp Head");
+        actors[1].setName("Derp Head Jr.");
+        return actors;
+    }
+    
+    private static Map createMap(int numRows, int numColumns) {
+        Map map = new Map();
+        return map;
+    }
+
 }
