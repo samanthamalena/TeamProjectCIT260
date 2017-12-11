46a4c329c751bbf979afc53ad035b0f3df6f869e
Me <Me@10.38.164.154>
2017-12-11 14:55:09 -0500

    Merge origin/master
    Conflicts:
    	TeamProjectCIT260/src/teamprojectcit260/TeamProjectCIT260.java

--- TeamProjectCIT260/src/model/BuildingScene.java
+++ TeamProjectCIT260/src/model/BuildingScene.java
@@ -6,74 +6,18 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Objects;
+import java.util.ArrayList;
 
 /**
  *
- * @author Me
+ * @author SamanthaMalena
  */
-public class BuildingScene implements Serializable {
-    private int numberOfSurvivors;
-    private int numberOfResources;
-    private Resource[] resources;
-
-    public Resource[] getResources() {
-        return resources;
-    }
-
-    public void setResources(Resource[] resources) {
-        this.resources = resources;
-    }
+public class BuildingScene extends Scene{
 
     public BuildingScene() {
+        setFoundMessage("");
     }
 
-    @Override
-    public String toString() {
-        return "BuildingScene{" + "numberOfSurvivors=" + numberOfSurvivors + ", numberOfResources=" + numberOfResources + '}';
-    }
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 79 * hash + this.numberOfSurvivors;
-        hash = 79 * hash + this.numberOfResources;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final BuildingScene other = (BuildingScene) obj;
-        if (this.numberOfSurvivors != other.numberOfSurvivors) {
-            return false;
-        }
-        if (this.numberOfResources != other.numberOfResources) {
-            return false;
-        }
-        return true;
-    }
-
-    public int getNumberOfSurvivors() {
-        return numberOfSurvivors;
-    }
-
-    public void setNumberOfSurvivors(int numberOfSurvivors) {
-        this.numberOfSurvivors = numberOfSurvivors;
-    }
-
-    public int getNumberOfResources() {
-        return numberOfResources;
-    }
-
-    public void setNumberOfResources(int numberOfResources) {
-        this.numberOfResources = numberOfResources;
-    }
 }
